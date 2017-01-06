05e4cb925386726d4ba72d8ea5a55eef7309526e
cantinos <cantinos.online@gmail.com>
2017-01-06 20:22:55 +0100

    test2

--- parse.py
+++ parse.py
@@ -2,7 +2,7 @@ from Bio import SeqIO
 from os import listdir
 from random import randint
 
-print("test")
+print("test2")
 
 #According to the chosen mode, access the desired folder and call the files reading functions
 #In the end, returns a list of dictionaries
